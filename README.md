db21d0baddfa25d44279a63f2d3fcb08899eec52
ramo828 <illegalism666@gmail.com>
2021-12-13 02:33:38 +0400

    add deb package android fixed 5

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Numb 1.8.4
+Numb 1.8.5
 
 Termux install
 
--- lib/numb.h
+++ lib/numb.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define __VERSION_APP__  "1.8.4 beta"
+#define __VERSION_APP__  "1.8.5 stable"
 void update();
 void reInstall();
 void removeApp();
