240b5f1dea305c45e212aea907407114bd26e293
ramo828 <illegalism666@gmail.com>
2022-02-24 01:02:37 +0300

    Fix robo

--- extension/Robo.java
+++ extension/Robo.java
@@ -26,16 +26,20 @@ public class Robo {
     private static String msg;
     private static Scanner data = new Scanner(System.in);
     private static String adress[] = {"https://raw.githubusercontent.com/ramo828/numb/main/extension/message/0",
-                                      "https://raw.githubusercontent.com/ramo828/numb/main/extension/message/1"
+                                      "https://raw.githubusercontent.com/ramo828/numb/main/extension/message/1",
+					  "https://raw.githubusercontent.com/ramo828/numb/main/extension/message/2"
+
                                         };
     private static int choise = 0;
     
     public static void main(String[] args) {
         try {
         if(args[0].equals("min"))
-            choise = 1;
+            	choise = 1;
+	else if(args[0].equals("099"))
+		choise = 2;
         } catch(ArrayIndexOutOfBoundsException ae){
-            choise = 0;
+            	choise = 0;
         }
         auto.setSourceData(adress[choise]);
         auto.setAlgoStatus(true);
